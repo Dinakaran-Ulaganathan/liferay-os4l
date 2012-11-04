6d23cf8248adac6e65f9bfae54944f2efd36a7d3
opensourceforlife <opensourceforlife@gmail.com>
2012-11-04 20:20:56 +0530

    Added Maven source for Custom Landing Page Hook
    Initial commit for Custom Landing Page Maven source. Also make
    security-manager-enabled to false, as doesn't look like it's working. So
    need to look into this later.

---
+++ Liferay/Plugins/Hook/CustomLandingPage-hook/maven/6.1.1CEGA2/CustomLandingPage-hook/src/main/java/com/liferay/opensourceforlife/landingpage/LandingPageType.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+package com.liferay.opensourceforlife.landingpage;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+
+/**
+ * @author tejas.kanani
+ */
+public abstract class LandingPageType {
+
+	public abstract String getLandingPagePath(
+		HttpServletRequest request, boolean includeLanguage)
+		throws PortalException, SystemException;
+}
